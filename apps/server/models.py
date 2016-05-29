f47a9f55478c041ea8f38498c49f1891b92f4c50
James Canning <james@jamescanning.com>
2016-05-29 20:45:39 +0100

    Snippet powered menus, used in header

--- apps/server/models.py
+++ apps/server/models.py
@@ -1,5 +1,11 @@
 from django.db import models
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
 from wagtail.contrib.settings.models import BaseSetting, register_setting
+from wagtail.wagtailadmin.edit_handlers import InlinePanel, FieldPanel, PageChooserPanel
+from wagtail.wagtailcore.models import Orderable, Page
+from wagtail.wagtailsearch import index
+from wagtail.wagtailsnippets.models import register_snippet
 
 
 class SingletonPage(object):
@@ -23,3 +29,34 @@ class AnalyticsSettings(BaseSetting):
     ga_property_id = models.CharField(
         max_length=20,
         help_text='UA-XXXXX-X')
+
+@register_snippet
+class Menu(ClusterableModel):
+    name = models.CharField(max_length=40)
+
+    panels = [
+        FieldPanel('name'),
+        InlinePanel('items')
+    ]
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(Orderable, models.Model):
+    page = ParentalKey(Page)
+    title = models.CharField(max_length=80)
+    menu = ParentalKey(Menu, related_name='items')
+
+    panels = [
+        PageChooserPanel('page'),
+        FieldPanel('title')
+    ]
+
+    def __str__(self):
+        return self.page.title + " -> " + self.menu.name
+
+
+@register_setting
+class MenuSettings(BaseSetting):
+    header_menu = ParentalKey(Menu, null=True)
