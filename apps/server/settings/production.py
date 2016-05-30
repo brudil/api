ce5fc64fe2b6d160fe3140d8c352d72ac9e95d67
James Canning <james@jamescanning.com>
2016-05-30 18:04:51 +0100

    Disable S3 signing

--- apps/server/settings/production.py
+++ apps/server/settings/production.py
@@ -10,6 +10,7 @@ DATABASES['default'].update(db_from_env)
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
+AWS_QUERYSTRING_AUTH = False
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
