826a4e3b7c971cd9c974bbb5c635db7b93d4c854
James Canning <james@jamescanning.com>
2016-05-30 17:54:34 +0100

    Add s3/boto storages 📦

--- apps/server/settings/production.py
+++ apps/server/settings/production.py
@@ -1,14 +1,18 @@
 from .base import *
 import dj_database_url
 import os
-import raven
 
 DEBUG = False
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 STATICFILES_DIRS = [
     ('assets', os.path.join(BASE_DIR, 'assets/dist')),
@@ -16,11 +20,12 @@ STATICFILES_DIRS = [
 
 SECRET_KEY = os.environ['APP_SECRET_KEY']
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['.herokuapp.com', ]
-
 
 INSTALLED_APPS += (
     'raven.contrib.django.raven_compat',
+    'storages',
 )
 
 RAVEN_CONFIG = {
@@ -32,6 +37,10 @@ RAVEN_CONFIG = {
 
 manifest = open(os.path.join(BASE_DIR, 'assets/dist/assets.json'))
 WEBPACK_ASSET_MANIFEST = json.load(manifest)
+
+STATIC_URL = '{}/static/'.format(os.environ['ASSETS_URL'])
+MEDIA_URL = '{}/media/'.format(os.environ['ASSETS_URL'])
+
 
 def asset_resolver(name):
     bundle, file = name.split('.')
