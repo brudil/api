55d86dd1a23380f1f0d68e0ca4818c5dffd3dec0
James Canning <james@jamescanning.com>
2016-06-02 16:31:42 +0100

    Add fetch pollyfill
    Closes #71

--- assets/src/js/schedule.js
+++ assets/src/js/schedule.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import FullSchedule from './components/FullSchedule';
+import 'whatwg-fetch';
 
 export default () => {
   const root = document.querySelector('#ScheduleFullRoot');
