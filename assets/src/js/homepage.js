98276b1c3676dfbdfb22e7ac67fff3b8532b6193
James Canning <james@jamescanning.com>
2016-10-02 18:07:26 +0100

    Prepare for homepage revamp

--- assets/src/js/homepage.js
+++ assets/src/js/homepage.js
@@ -1,10 +1,2 @@
-import { lory } from 'lory.js';
-
 export default () => {
-  const slider = document.querySelector('.js_slider');
-  if (slider) {
-    lory(slider, {
-      infinite: 1,
-    });
-  }
 };
